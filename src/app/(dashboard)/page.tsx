2efe6ac73835f420285157c418f45ef61880d70c
sumitkudale21 <sumitkudale3545@gmail.com>
2025-07-06 15:07:53 +0530

    trpc setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -6,6 +6,9 @@ import { auth } from "@/lib/auth";
 import { HomeView } from "@/modules/home/ui/views/home-view"
 
 const Page = async () => {
+
+ 
+
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -13,6 +16,7 @@ const Page = async () => {
   if (!session) {
     redirect("/sign-in");
   }
+
   return <HomeView />
 };
 
